d01f83151f909960cfaf79066df5270bd3e0abd8
Sergey Nazarov <ric19902002@icloud.com>
2025-06-17 16:27:01 +0300

    the therd commit in branch test

--- Example1/src/main/java/org/example/App.java
+++ Example1/src/main/java/org/example/App.java
@@ -31,5 +31,8 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
+        for (int i = 0; i < 100; i++) {
+
+        }
     }
 }
