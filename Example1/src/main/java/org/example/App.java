f696f3b62cb139a9604fdcc718cbb14aba1c14f1
Sergey Nazarov <ric19902002@icloud.com>
2025-06-17 16:18:51 +0300

    the first commit in branch test2

--- Example1/src/main/java/org/example/App.java
+++ Example1/src/main/java/org/example/App.java
@@ -2,34 +2,7 @@ package org.example;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
 
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-
-        ystem.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-
-        ystem.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-
-        ystem.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
     }
 }
